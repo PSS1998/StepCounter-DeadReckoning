318c7c031afda4e6e432d0f7f685386d23030e78
PSS1998 <p_sadri_s@yahoo.com>
2021-06-22 01:14:20 +0430

    Add Magnetic Declination to orientation

--- app/src/main/java/com/example/stepcounter/Orientation.java
+++ app/src/main/java/com/example/stepcounter/Orientation.java
@@ -2,6 +2,8 @@ package com.example.stepcounter;
 
 import android.hardware.SensorManager;
 
+import java.util.GregorianCalendar;
+
 public class Orientation {
     private float[] orientationAngles = new float[3];
     private float[] rotationMatrix = new float[9];
@@ -25,10 +27,20 @@ public class Orientation {
         SensorManager.getRotationMatrix(rotationMatrix, null,
                 Accelerometer.getInstance(sensorManager).getAcceleration(), Magnetometer.getInstance(sensorManager).getMagnetic_field());
         SensorManager.getOrientation(rotationMatrix, orientationAngles);
-
+        orientationAngles[0] += calculateMagneticDeclination();
+        orientationAngles[0] = (float)((orientationAngles[0]>Math.PI) ? (orientationAngles[0] - 2*Math.PI) : (orientationAngles[0]<-Math.PI) ? (orientationAngles[0] + 2*Math.PI) : orientationAngles[0]);
     }
 
     public float[] getOrientationAngles() {
         return orientationAngles;
     }
+
+    // TODO: Get lat, long and altitude values from Location Service
+    private static float calculateMagneticDeclination() {
+        TSAGeoMag geoMag = new TSAGeoMag();
+        double declination = geoMag.getDeclination(35.6892, 51.3890, geoMag.decimalYear(new GregorianCalendar()), 1.189);
+        declination = Math.toRadians(declination);
+        return (float)declination;
+    }
+
 }
