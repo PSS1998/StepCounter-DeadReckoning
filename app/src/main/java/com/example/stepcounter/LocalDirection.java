d0ea78d546ce05f1895bcaae3058300f5d11fd6f
PSS1998 <p_sadri_s@yahoo.com>
2021-06-21 18:12:11 +0430

    Add GAME_ROTATION_VECTOR sensor support for localDirection

--- app/src/main/java/com/example/stepcounter/LocalDirection.java
+++ app/src/main/java/com/example/stepcounter/LocalDirection.java
@@ -7,14 +7,12 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.widget.Toast;
 
-import com.example.stepcounter.services.StepCounterService;
-
-import java.util.ArrayList;
 
 public class LocalDirection implements SensorEventListener {
 
     private SensorManager mySensorManager;
     Sensor gyroscopeSensor;
+    Sensor rotationSensor;
 
     private final static int BUFFER_LEN = 64;
     // Ring Buffer
@@ -33,26 +31,31 @@ public class LocalDirection implements SensorEventListener {
     private static float estimatedTurn = 0;
     private static float lastEstimatedTurn = 0;
     private static float initialHeading;
+    private static float initialHeadingBias = 0;
     private static float currentHeading;
     private static Context contextt;
     Orientation orientation;
     int initialHeadingSet = 0;
+    private static float mRotationMatrix[] = new float[16];
+    int counterCompass = 16;
 
     public LocalDirection(Context context){
         mySensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        gyroscopeSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        if (gyroscopeSensor == null){
+
+        rotationSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        if (rotationSensor == null){
+            gyroscopeSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            if (gyroscopeSensor == null){
+            }else{
+                mySensorManager.registerListener(this, gyroscopeSensor, SensorManager.SENSOR_DELAY_GAME);
+            }
         }else{
-            mySensorManager.registerListener(this, gyroscopeSensor, SensorManager.SENSOR_DELAY_GAME);
+            mySensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_GAME);
         }
 
         contextt = context;
 
         orientation = Orientation.getInstance(mySensorManager);
-        orientation.updateOrientationAngles();
-        float[] orientationAngles = orientation.getOrientationAngles();
-        initialHeading = orientationAngles[0];
-        currentHeading = initialHeading;
     }
 
     // Ring buffer subroutines
@@ -78,8 +81,78 @@ public class LocalDirection implements SensorEventListener {
         return (float)(sum);
     }
 
+    public float getBufferMinusSum(){
+        float sum = 0;
+        for( int i=buffTail; i!=Math.floorMod((buffHead-1),BUFFER_LEN); i=(i+1)%BUFFER_LEN ){
+            sum += gyroBuffer[i]-gyroBuffer[(i+1)%BUFFER_LEN];
+        }
+        return (float)(sum);
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event){
+        if (event.sensor.getType()==Sensor.TYPE_GAME_ROTATION_VECTOR){
+            float orientationVals[] = {0, 0, 0};
+            // Convert the rotation-vector to a 4x4 matrix.
+            SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+            SensorManager.getOrientation(mRotationMatrix, orientationVals);
+
+            orientationVals[0] -= initialHeadingBias;
+            orientationVals[0] = (float)((orientationVals[0] < 0) ? (orientationVals[0] + (2.0 * Math.PI)) : orientationVals[0]);
+
+            if (timestamp == 0)
+                timestamp = event.timestamp;
+            gyroTimeStamp = (event.timestamp - timestamp) * NS2S;
+            timestamp = event.timestamp;
+
+            if((initialHeadingSet == 0)){
+                orientation.updateOrientationAngles();
+                float[] orientationAnglesMagnetic = orientation.getOrientationAngles();
+                initialHeading = orientationAnglesMagnetic[0];
+                initialHeading = (float)((initialHeading < 0) ? (initialHeading + (2.0 * Math.PI)) : initialHeading);
+                if(initialHeading != 0.0){
+                    counterCompass -= 1;
+                    if(counterCompass == 0) {
+                        initialHeadingBias = orientationVals[0];
+                        initialHeadingSet = 1;
+                    }
+                }
+            }
+
+            currentHeading = initialHeading+orientationVals[0];
+
+            currentHeading = (float)((currentHeading > 3*Math.PI) ? (currentHeading - 4.0 * Math.PI) : (currentHeading > Math.PI) ? (currentHeading - 2.0 * Math.PI) : (currentHeading < -Math.PI) ? (currentHeading + 2.0 * Math.PI) : currentHeading);
+
+            // add the Y-axis gyro component to the buffer
+            addToBuffer(orientationVals[0], gyroTimeStamp);
+
+            // calculate the current cumulative sum
+            float bufferSum = getBufferMinusSum();
+
+            estimatedTurn = bufferSum;
+
+            // only try to calculate turns if we're not in backoff
+            if( backoffTimer <= 0 ){
+                if( Math.abs(bufferSum) > TURN_MINIMUM){
+                    float lastTwoTurnRadian = lastEstimatedTurn+estimatedTurn;
+                    if(Math.abs(Math.abs(lastTwoTurnRadian) - Math.abs(2*Math.PI)) < 0.4*Math.PI){
+                        Toast.makeText(contextt, "360 degree turn detected", Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        if (Math.abs(Math.abs(lastTwoTurnRadian) - Math.abs(Math.PI)) < 0.2 * Math.PI) {
+                            Toast.makeText(contextt, "180 degree turn detected", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    lastEstimatedTurn = estimatedTurn;
+                    // backoff
+                    backoffTimer = TURN_BACKOFF;
+                }
+
+            }else{
+                backoffTimer--;
+            }
+
+        }
         if (event.sensor.getType()==Sensor.TYPE_GYROSCOPE){
             // grab current data and throw it into an array
             gyroCurrentValue[0] = event.values[0];
@@ -91,19 +164,21 @@ public class LocalDirection implements SensorEventListener {
             gyroTimeStamp = (event.timestamp - timestamp) * NS2S;
             timestamp = event.timestamp;
 
-            if((initialHeading == 0.0) && (initialHeadingSet == 0)){
+            if((initialHeadingSet == 0)){
                 orientation.updateOrientationAngles();
-                float[] orientationAngles = orientation.getOrientationAngles();
-                initialHeading = orientationAngles[0];
-                currentHeading = initialHeading;
+                float[] orientationAnglesMagnetic = orientation.getOrientationAngles();
+                initialHeading = orientationAnglesMagnetic[0];
                 if(initialHeading != 0.0){
-                    initialHeadingSet = 1;
+                    counterCompass -= 1;
+                    if(counterCompass == 0) {
+                        currentHeading = initialHeading;
+                        initialHeadingSet = 1;
+                    }
                 }
             }
 
-            currentHeading -= gyroTimeStamp*gyroCurrentValue[1];
+            currentHeading -= gyroTimeStamp*gyroCurrentValue[2];
 
-            // TODO: decide Y-axis or Z-axis should be used here based on inclination of device
             // add the Y-axis gyro component to the buffer
             addToBuffer(gyroCurrentValue[1], gyroTimeStamp);
 
@@ -116,13 +191,14 @@ public class LocalDirection implements SensorEventListener {
             if( backoffTimer <= 0 ){
                 if( Math.abs(bufferSum) > TURN_MINIMUM){
                     float lastTwoTurnRadian = lastEstimatedTurn+estimatedTurn;
-                    if(Math.abs(Math.abs(lastTwoTurnRadian) - Math.abs(Math.PI)) < 0.2*Math.PI){
-                        Toast.makeText(contextt, "180 degree turn detected", Toast.LENGTH_LONG).show();
-                    }
-                    if(Math.abs(Math.abs(lastTwoTurnRadian) - Math.abs(2*Math.PI)) < 0.2*Math.PI){
+                    if(Math.abs(Math.abs(lastTwoTurnRadian) - Math.abs(2*Math.PI)) < 0.4*Math.PI){
                         Toast.makeText(contextt, "360 degree turn detected", Toast.LENGTH_LONG).show();
                     }
-//                    currentHeading += estimatedTurn;
+                    else {
+                        if (Math.abs(Math.abs(lastTwoTurnRadian) - Math.abs(Math.PI)) < 0.2 * Math.PI) {
+                            Toast.makeText(contextt, "180 degree turn detected", Toast.LENGTH_LONG).show();
+                        }
+                    }
                     lastEstimatedTurn = estimatedTurn;
                     // backoff
                     backoffTimer = TURN_BACKOFF;
